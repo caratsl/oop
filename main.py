a89f8f93a44f285c8747677c1e683b27d4a0a7ef
caratsl <104669971+caratsl@users.noreply.github.com>
2022-11-17 22:36:55 +0700

    1 lab

---
+++ main.py
@@ -0,0 +1,16 @@
+from vehicles import *
+
+
+
+file_in()
+if len(objects) == 0:
+    print("Container is empty")
+else:
+    print("Container is filled")
+    print(f"Contains {len(objects)} element(s)")
+    for i in objects:
+        i.print_info(i)
+    clear_container()
+    print("Container is empty")
+    print(f"Contains {len(objects)} element(s)")
+
---
+++ vehicles.py
@@ -0,0 +1,101 @@
+def gen_id(arr):
+    ids = 0
+    check = True
+    while check:
+        check_tmp = False
+        for i in arr:
+            if i.get_id(i) == ids:
+                check_tmp = True
+        if check_tmp:
+            ids = ids + 1
+        else:
+            check = False
+    return ids
+
+
+objects = []
+
+class Vehicle:
+    def __init__(self):
+        self.power = 0
+
+    def set_power(self,in_power):
+        self.power = in_power
+
+    def get_type(self):
+        return self.type
+
+
+
+class Bus(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        self.capacity = 0
+        self.type = 'BUS'
+        self.id = 0
+
+    def set_id(self,tmp):
+        self.id = tmp
+
+    def get_id(self):
+        return self.id
+
+    def set_capacity(self,cap):
+        self.capacity = cap
+
+    def print_info(self):
+        print(f'{self.get_id(self)}: It is BUS, Power = {self.power}, capacity = {self.capacity}')
+
+
+class Truck(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        self.maxWeight = 0
+        self.type = 'TRUCK'
+        self.id = 0
+
+    def set_id(self,tmp):
+        self.id = tmp
+
+    def get_id(self):
+        return self.id
+
+    def set_maxWeight(self,mw):
+        self.maxWeight = mw
+
+    def print_info(self):
+        print(f'{self.get_id(self)}: It is TRUCK, Power = {self.power}, maxWeight = {self.maxWeight}')
+
+def file_in():
+    f = open('in.txt')
+    check = True
+    while check:
+        text = f.readline()
+        if text == '':
+            check = False
+        else:
+            text = text.split('\n')
+            type = int(text[0])
+            if type == 1:  # это BUS
+                veh = Bus
+                text = f.readline()
+                a = text.split(' ')
+                power = int(a[0])
+                tmp = int(a[1])
+                veh.set_power(veh,in_power=power)
+                veh.set_capacity(veh,cap=tmp)
+                veh.set_id(veh,tmp= gen_id(objects))
+                objects.append(veh)
+            elif type == 2:
+                veh = Truck
+                text = f.readline()
+                a = text.split(' ')
+                power = int(a[0])
+                tmp = int(a[1])
+                veh.set_power(veh,in_power=power)
+                veh.set_maxWeight(veh,mw=tmp)
+                veh.set_id(veh, tmp=gen_id(objects))
+                objects.append(veh)
+
+def clear_container():
+    objects.clear()
